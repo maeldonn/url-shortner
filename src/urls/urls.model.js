02e03308887e8621e98f02609f405289e4f1d4d9
Maël Donnart <57325246+maeldonn@users.noreply.github.com>
2020-09-16 19:31:23 +0200

    :bug: Updated mongoose schema

--- src/urls/urls.model.js
+++ src/urls/urls.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Url = mongoose.Schema(
   {
     url: { type: String, required: true },
-    slug: { type: String, required: true },
+    slug: { type: String, unique: true, required: true },
   },
   {
     versionKey: false,
